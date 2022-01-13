cc87e7a0808905b508fe4e93f5a09116cb770413
dns <pochti_zvezda@mail.ru>
2022-01-13 23:06:05 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/calculatortest/CalculatorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calculatortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
